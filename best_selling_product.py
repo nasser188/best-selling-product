cbd25ef289d47178100ebd1506b653f51582dd11
nasser188 <vghh91601@gmail.com>
2026-01-04 15:43:07 +0400

    Update best_selling_product.py

--- best_selling_product.py
+++ best_selling_product.py
@@ -1,12 +1,13 @@
 products = ["Product A", "Product B", "Product C"]
 sales = [1000, 2500, 1500]
 
-maxSales = 0
-bestProduct = ""
+counter = 0
 
 for i in range(len(products)):
+    counter += 1
     if sales[i] > maxSales:
         maxSales = sales[i]
         bestProduct = products[i]
 
 print("Best-selling product:", bestProduct)
+
